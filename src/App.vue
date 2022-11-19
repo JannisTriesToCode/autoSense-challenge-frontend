0cfebc331f50f4335d14190dae5fc73e9a78a2ab
Jannis Rauschke <jannis@rauschke.eu>
2022-11-19 14:02:39 +0100

    Use mounted hook to load all fuelstations

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <ModifyFuelstation />
+      <GetAllFullstationsVue />
     </v-main>
   </v-app>
 </template>
--- src/components/GetAllFullstations.vue
+++ src/components/GetAllFullstations.vue
@@ -1,9 +1,5 @@
 <template>
     <v-responsive class="mx-auto" max-width="344">
-        <v-btn @click="loadFuelstations()" variant="tonal"
-          >Load fuelstations</v-btn
-        >
-
         <div v-for="f in fuelstations">
            {{ f.id }} - {{ f.name }}
         </div>
@@ -12,24 +8,27 @@
   
 <script>
 export default {
-data: () => ({
-    fuelstations: []
-}),
-methods: {
-    async loadFuelstations() {
-    await fetch(process.env.BACKEND_IP + "/fuelstations", {
-        method: "GET",
-        headers: {
-        "API-Key": process.env.API_KEY,
+    data: () => ({
+        fuelstations: []
+    }),
+    methods: {
+        async loadFuelstations() {
+            await fetch(process.env.BACKEND_IP + "/fuelstations", {
+                method: "GET",
+                headers: {
+                "API-Key": process.env.API_KEY,
+                },
+            })
+            .then((response) => response.json())
+            .then((data) => this.fuelstations = data.data)
+            .catch((error) => {
+            console.log(error);
+            });
         },
-    })
-        .then((response) => response.json())
-        .then((data) => this.fuelstations = data.data)
-        .catch((error) => {
-        console.log(error);
-        });
     },
-},
+    mounted () {
+        this.loadFuelstations()
+    }
 };
 </script>
 
